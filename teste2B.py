635841de41f5a484e4bf85c8c957bdd8f4881079
lucas.sola <lucas.sola@aluno.senai.br>
2025-04-02 12:28:46 -0300

    Update teste2B.py

--- teste2B.py
+++ teste2B.py
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
 print("Hello world!")
 print("O daniel é lindo e pronto!")
 print("O Marlon tá cansado!")
+=======
+print("Hello world!")
+print("O daniel é legal")
+>>>>>>> Stashed changes
